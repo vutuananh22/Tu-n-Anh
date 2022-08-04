4200c867f10e90657b5c30b2e7e9b13496208e75
vutuananh22 <110403011+vutuananh22@users.noreply.github.com>
2022-08-04 12:15:24 +0700

    Create bai tap 2.cpp

---
+++ bai tap 2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int num;
+	printf("nhap num=");
+	scanf("%d",&num);
+	if(num>0)
+			printf("%d la so duong",num);
+        else
+		if(num<0)
+				printf("%d la so am",num);
+		else
+			printf("%d khong phai la so duong khong phai la so am",num);	
+		
+		
+	
+	
+
+}
+	
