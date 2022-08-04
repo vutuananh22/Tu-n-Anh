6e36d1ea9052e78d0048d54f2a9d89b58d5feabc
vutuananh22 <110403011+vutuananh22@users.noreply.github.com>
2022-08-04 12:15:32 +0700

    Create bai tap 3.cpp

---
+++ bai tap 3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	//input a
+	int a;
+	printf("nhap a=");
+	scanf("%d",&a);
+	
+	//input b
+	int b;
+	
+	printf("nhap b=");
+	scanf("%d",&b);
+	
+	if(a>b)
+	printf("%d laf so lon nhat",a);
+	else
+	if(a<b)
+	printf("%d la so lon nhat",b);
+}
