b62e0ece52009b6326a017ec9cd965a8f1c03237
vutuananh22 <110403011+vutuananh22@users.noreply.github.com>
2022-08-08 16:57:05 +0700

    Create bai tap buoi hoc 8.cpp

---
+++ bai tap buoi hoc 8.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int S = 0;
+	int y = 0;
+	int n;
+	printf("nhap n=");
+	scanf("%d",&n);
+	while(y<=n){
+		// yes 
+			printf("tong so\n");
+		int S=S+y;
+		y++;
+		
+	}
+	// no
+	printf("tong S=%d",&S);
+}
