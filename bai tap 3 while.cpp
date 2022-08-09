1a3a45466dbaab234a9890054687b735ab042a4c
vutuananh22 <110403011+vutuananh22@users.noreply.github.com>
2022-08-09 17:46:44 +0700

    Create bai tap 3 while.cpp

---
+++ bai tap 3 while.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	int n = 0; 
+        while(n<=0){
+        	
+		        
+	printf("nhap vao n> 0:");
+	scanf("%d",&n);
+}
+	int i;{
+	printf(" cac so uoc cua n:");
+	for( i= 1;i<=sqrt(n);i++);{
+	if(n % i == 0);
+	printf("%5d",i);
+	printf("%5d", n / i);
+}
+	}
+	
+}
+
+
+	
