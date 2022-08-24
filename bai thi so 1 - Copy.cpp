2f644a04df9adfa722b693d64a9b6a6854cb530b
vutuananh22 <110403011+vutuananh22@users.noreply.github.com>
2022-08-24 15:49:19 +0700

    Create bai thi so 1 - Copy.cpp

---
+++ bai thi so 1 - Copy.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+	int n;
+	float sum = 0;
+	
+	do{
+	printf("nhap vao n=");
+	scanf("%d",&n);
+	if(n<1){          // nho hon 1 thi nhap lai!
+		printf("so nhap vao khong hop le vui long nhap lai!");
+	}
+	
+    }while(n<1);  
+        for(int i=1;i<=n;i++){
+        	sum=sum+1.0/i;     
+		}
+		
+		printf("\nS = 1 + 2 + 1/3 +....+1/%d la : %.2f",n,sum);
+		
+     return 0;
+}
