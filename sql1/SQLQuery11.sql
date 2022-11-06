143f031078946916b1c66836f1aeda253408f493
vutuananh22 <110403011+vutuananh22@users.noreply.github.com>
2022-11-06 22:40:03 +0700

    Update SQLQuery11.sql

---
+++ sql1/SQLQuery11.sql
@@ -0,0 +1,14 @@
+﻿insert into ThongTinKhachHang(ten,cmtnd,diachi)
+values (N'Nguyễn Nguyệt NGA',123456789,N'Hà Nội');
+
+select * from ThongTinKhachHang;
+
+insert into ThueBaoKhachHang(so,loai,ngaydangky)
+values (123456789,N'Trả trước','02/12/12');
+
+select * from ThueBaoKhachHang;
+
+insert into ThongTincuaThueBao(cmtKH,soTB)
+values (123456789,123456789);
+
+select * from ThongTincuaThueBao;
